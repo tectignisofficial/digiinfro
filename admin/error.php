627f708d75530edd83611af27e81d93fe4386fb7
priya8425 <yadavpriya1425@gmail.com>
2022-06-29 12:19:01 +0530

    div

--- admin/error.php
+++ admin/error.php
@@ -113,12 +113,7 @@ if(isset($_POST["404submit"])){
           <div class="col-sm-6">
           
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
+          <!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
@@ -130,7 +125,7 @@ if(isset($_POST["404submit"])){
 <div class="container-fluid">
 <div class="row">
 <div class="col-md-12">
-
+<div class="card card-primary">
 <div class="m-4">
     <ul class="nav nav-tabs" id="myTab">
         <li class="nav-item">
@@ -251,7 +246,7 @@ if(isset($_POST["404submit"])){
             </div>
                   </div>
                   </div>
-                  
+                  </div>
     </section>
     <!-- /.content -->
   </div>
