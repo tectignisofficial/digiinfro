9c4762a402b552adb520df879cee643818df50ae
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-07-07 12:31:53 +0530

    s

---
+++ admin/include/config.php
@@ -0,0 +1,6 @@
+<?php
+$conn=mysqli_connect("151.106.124.51","u188140722_digiinfroma","@Dm!n$2025","u188140722_digiinfroma");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+?>
