946464734136ed6fbc7c765346c576c91925372f
Govind Bavkar <tectignisofficial@gmail.com>
2022-07-09 10:54:19 +0530

    p

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -128,12 +128,6 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="partners.php" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Partners</p>
-            </a>
-          </li>
-          <li class="nav-item">
             <a href="offer.php" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
               <p>Offer</p>
