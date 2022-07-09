545a9ae772b3d491a808d48cb8f794261efc64f5
Govind Bavkar <tectignisofficial@gmail.com>
2022-07-09 11:46:55 +0530

    cc

--- adminlogin/index.php
+++ adminlogin/index.php
@@ -71,7 +71,8 @@ else{
                     <label for="password" class="sr-only">Password</label>
                     <input type="password" name="password" id="password" class="form-control" placeholder="***********">
                   </div>
-                  <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="login">
+                  <!-- <button name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="login"></button> -->
+                  <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="login" name="login" id="login" value="login">Login</button>
                 </form>
                 <!-- <a href="#!" class="forgot-password-link">Forgot password?</a>
                 <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
