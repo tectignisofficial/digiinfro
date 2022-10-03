0f4991477fe9cb446399be2446848ac9ca26a2da
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-03 11:52:24 +0530

    d

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-inline-block">
-      Product by <a href="https://tectignis.in/" target="_blank"> <b>Tectignis IT Solutions</b></a>
+      Product by <a href="https://tectignis.in/" target="_blank" style="color:#869099"> <b>Tectignis IT Solutions</b></a>
     </div>
   </footer>
